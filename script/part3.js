da5aad9870b23ea4f2fafd7144b80b97e53cc7b1
UnicusStella <unicusstella@gmail.com>
2021-08-27 15:31:11 +0900

    mysql part-3 Complete

--- script/part3.js
+++ script/part3.js
@@ -9,11 +9,11 @@ const PART3_1 = `SHOW DATABASES`;
     TODO: Q 3-2. user 테이블의 구조를 보기위한 SQL을 작성해주세요.
         - 요구사항에 맞는 user 테이블을 작성해야만, 테스트를 통과합니다.
 */
-const PART3_2 = `SELECT* FROM user`;
+const PART3_2 = `desc user`;
 /*
     TODO: Q 3-3. content 테이블의 구조를 보기위한 SQL을 작성해주세요.
         - 요구사항에 맞는 content 테이블을 작성해야만, 테스트를 통과합니다.
 */
-const PART3_3 = `SELECT * FROM content`;
+const PART3_3 = `desc content`;
 
 module.exports = { PART3_1, PART3_2, PART3_3 };
