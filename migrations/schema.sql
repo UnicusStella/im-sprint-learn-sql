70c1806cfb192bb9849e9b3d2ca27f6b29b41d9c
UnicusStella <unicusstella@gmail.com>
2021-08-30 15:44:18 +0900

    modify

--- migrations/schema.sql
+++ migrations/schema.sql
@@ -24,22 +24,31 @@ CREATE TABLE `content` (
 -- PART 5:
 -- 아래 주석을 제거하고 category, content_category, role 테이블을 만드세요.
 
-/*
+
 CREATE TABLE `category` (
   -- TODO:
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(255) not NULL
 );
 
 
 CREATE TABLE `content_category` (
   -- TODO:
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `contentId` int not NULL,
+  `categoryId` int not NULL,
+  FOREIGN KEY (`contentId`) REFERENCES `content` (`id`),
+  FOREIGN KEY (`categoryId`) REFERENCES `category` (`id`),
+  `created_at` timestamp not NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 
 CREATE TABLE `role` (
   -- TODO:
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(255) not NULL
 );
 
 
 ALTER TABLE `user` ADD roleId int;
 ALTER TABLE `user` ADD FOREIGN KEY (`roleId`) REFERENCES `role` (`id`);
-*/
